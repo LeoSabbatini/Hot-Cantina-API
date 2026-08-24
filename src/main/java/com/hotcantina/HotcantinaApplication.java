be4b93944c6b73bb7501aebe7146f2f4528e145e
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-08-24 08:28:31 -0300

    feat: estrutura inicial

---
+++ src/main/java/com/hotcantina/HotcantinaApplication.java
@@ -0,0 +1,13 @@
+package com.hotcantina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotcantinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotcantinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotcantina/HotcantinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotcantina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotcantinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
